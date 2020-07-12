28d5be3c6ffbdcb2ac70d00e92eefc1b91ca0147
ManhNguyen <manhnguyen2161998@gmail.com>
2020-07-12 19:01:46 +0700

    manh update demo 1

--- app/src/main/java/com/example/cloneshopee/home/displayMe/FragmentMe.kt
+++ app/src/main/java/com/example/cloneshopee/home/displayMe/FragmentMe.kt
@@ -1,9 +1,11 @@
 package com.example.cloneshopee.home.displayMe
 
 
+import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -35,6 +37,19 @@ class FragmentMe : Fragment() {
             editor.remove("rememberMe")
             editor.apply()
             startActivity(intent)
+        }
+
+        meDisplayBinding.txtvNavToUngdungchuquan.setOnClickListener {
+            val intent = activity!!.applicationContext.packageManager.getLaunchIntentForPackage("com.example.storeapplication")
+            val sharedPreferences = activity!!.getSharedPreferences("CurrentUser", 0)
+            val currentUser = sharedPreferences.getString("currentuser", "")
+            if(intent != null){
+                intent.putExtra("test", currentUser)
+                startActivity(intent)
+                activity!!.finish()
+            }else{
+                Log.i("Death","Error when lauching")
+            }
         }
     }
 }
